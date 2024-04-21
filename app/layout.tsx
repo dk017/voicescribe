5aca4e410d7bc04eee18828f5b7b65347fd96083
dk017 <dhineshkumar.stoic@gmail.com>
2024-04-21 14:38:08 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "Voice Scribe Hey",
+  description: "AI-powered article generator from Voice",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+    signInFallbackRedirectUrl={"/"}
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!} appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          <main>
+          {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
